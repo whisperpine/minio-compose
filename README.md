5a39bfda351bd79f24b48ab5f3ca0a451acacd24
whisperpine <yusong.lai@icloud.com>
2024-03-20 12:52:23 +0800

    feat: add helper shell scripts

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 Deploy [MinIO](https://min.io/) with Docker Compose.
 
-## Environment Variables
-
-### Docker Compose
+## Docker Compose
 
 Edit `.env` to configure env vars available in `compose.yaml`.\
-Copy  [template.env](./template.env) to get started.
+Duplicate [template.env](./template.env) as `.env` to get started.
 
-### Nginx
+## Nginx
 
 By default, template files in `/etc/nginx/templates/*.template` will be read\
 and the result of executing `envsubst` will be output to `/etc/nginx/conf.d/`.\
 See more in [Using environment variables in nginx configuration (new in 1.19)](https://hub.docker.com/_/nginx#:~:text=Using%20environment%20variables%20in%20nginx%20configuration%20(new%20in%201.19)).
+
+## Certbot
+
+## Cloudflare Tunnel
---
+++ apply-tls.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# run this script to apply free tls certs.
+
+# get env vars.
+. ./.env
+# echo $MINIO_CONSOLE_DOMAIN
+# echo $MINIO_API_DOMAIN
+
+red_echo() {
+    echo -e "\033[31m$@\033[0m"
+}
+green_echo() {
+    echo -e "\033[32m$@\033[0m"
+}
+
+######### dry-run #########>
+
+sudo $DOCKER_COMPOSE run --rm \
+    certbot certonly --webroot \
+    --webroot-path /var/www/certbot/ \
+    --agree-tos \
+    --no-eff-email \
+    -m $LETSENCRYPT_EMAIL \
+    -d $MINIO_CONSOLE_DOMAIN \
+    --dry-run
+
+if [ $? -ne 0 ]; then
+    red_echo "failed to apply tls certificates for:"
+    red_echo "$MINIO_CONSOLE_DOMAIN"
+    exit 1
+fi
+
+sudo $DOCKER_COMPOSE run --rm \
+    certbot certonly --webroot \
+    --webroot-path /var/www/certbot/ \
+    --agree-tos \
+    --no-eff-email \
+    -m $LETSENCRYPT_EMAIL \
+    -d $MINIO_API_DOMAIN \
+    --dry-run
+
+if [ $? -ne 0 ]; then
+    red_echo "failed to apply tls certificates for:"
+    red_echo "$MINIO_API_DOMAIN"
+    exit 1
+fi
+######### dry-run #########<
+
+######### rm dummy files #########>
+sudo $DOCKER_COMPOSE exec nginx rm -r /etc/nginx/ssl/live
+green_echo "dummy tls certifates have been deleted"
+######### rm dummy files #########<
+
+######### apply #########>
+
+sudo $DOCKER_COMPOSE run --rm \
+    certbot certonly --webroot \
+    --webroot-path /var/www/certbot/ \
+    --agree-tos \
+    --no-eff-email \
+    -m $LETSENCRYPT_EMAIL \
+    -d $MINIO_CONSOLE_DOMAIN
+
+if [ $? -ne 0 ]; then
+    red_echo "failed to apply tls certificates for:"
+    red_echo "$MINIO_CONSOLE_DOMAIN"
+    exit 1
+fi
+
+sudo $DOCKER_COMPOSE run --rm \
+    certbot certonly --webroot \
+    --webroot-path /var/www/certbot/ \
+    --agree-tos \
+    --no-eff-email \
+    -m $LETSENCRYPT_EMAIL \
+    -d $MINIO_API_DOMAIN
+
+if [ $? -ne 0 ]; then
+    red_echo "failed to apply tls certificates for:"
+    red_echo "$MINIO_API_DOMAIN"
+    exit 1
+fi
+
+######### apply #########<
+
+######### reload nginx #########<
+sudo $DOCKER_COMPOSE exec nginx nginx -s reload
+
+if [ $? -ne 0 ]; then
+    red_echo ":: failed to reload nginx"
+    exit 1
+fi
+######### reload nginx #########<
